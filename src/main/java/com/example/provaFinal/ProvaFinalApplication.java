dc9587184a0522768558144595a77b2568e52bfe
LGHARO <lgharo06@gmail.com>
2026-05-12 16:35:55 -0300

    Initial commit

---
+++ src/main/java/com/example/provaFinal/ProvaFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.provaFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaFinalApplication.class, args);
+	}
+
+}
